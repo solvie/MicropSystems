a717035a6a15130274d08f6c18bcfbc19ec62009
Ryan Wang <luoqing.wang@mail.mcgill.ca>
2017-10-31 13:45:08 -0400

    initial commit Lab3

---
+++ Lab3_STM32F4_Base_project/MDK-ARM/RTE/_lab3_Configuration/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'lab3' 
+ * Target:  'lab3 Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
